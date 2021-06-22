af6601f6f9bb69dcd3dcd42d47ec64a35c47a81d
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-22 16:44:11 +0800

    06_22 on project_seadrone

--- docker/laptop/docker_run.sh
+++ docker/laptop/docker_run.sh
@@ -44,13 +44,13 @@ docker run -it \
   -e QT_X11_NO_MITSHM=1 \
   -e XAUTHORITY=$XAUTH \
   -v "$XAUTH:$XAUTH" \
-  -v "/home/$USER/seadrone-project:/home/seadrone/seadrone-project" \
+  -v "/home/$USER/project_seadrone:/home/seadrone/project_seadrone" \
   -v "/tmp/.X11-unix:/tmp/.X11-unix" \
   -v "/etc/localtime:/etc/localtime:ro" \
   -v "/dev:/dev" \
   -v "/var/run/docker.sock:/var/run/docker.sock" \
   -v "/home/$USER/.bashrc:/home/seadrone/.bashrc" \
-  --name seadrone-project \
+  --name project_seadrone \
   --network host \
   --rm \
   $DOCKER_OPTS \
