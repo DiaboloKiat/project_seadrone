1e90b03f439c0aee10e1a5556094fb836ce7ca8d
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-22 15:59:22 +0800

    06_22 on project_seadrone

---
+++ environment.sh
@@ -0,0 +1,3 @@
+source ~/project_seadrone/catkin_ws/devel/setup.bash
+source /opt/ros/melodic/setup.bash --extend
+
---
+++ github/git-pull-master.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# echo "password: $2"
+git checkout master
+BRANCH=master
+if [ ! -z "$1" ]; then
+    echo "pull branch: $1"
+    BRANCH=$1
+fi
+
+echo "--------------------------------------------------------------------------------"
+echo "---------------------------pull project_seadrone--------------------------------"
+echo "--------------------------------------------------------------------------------"
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in project_seadrone. Aborting"
+   return 1
+fi
+
+echo "-----------------------------------------------------------------------"
+echo "--------------------------pull seadrone_ros----------------------------"
+echo "-----------------------------------------------------------------------"
+cd ~/project_seadrone/catkin_ws/src/seadrone_ros
+git checkout $BRANCH
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in seadrone_ros. Aborting"
+   return 1
+fi
+
+cd ~/project_seadrone
+return 0
