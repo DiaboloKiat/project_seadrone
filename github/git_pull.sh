e9b5c4c243cdd73f059bb24d4be68b1eda31d9cd
DiaboloKiat <DiaboloKiat@gmail.com>
2021-07-08 20:29:57 +0800

    update on project_seadrone

--- github/git_pull.sh
+++ github/git_pull.sh
@@ -60,11 +60,11 @@ if [ "$CONFLICTS" -gt 0 ] ; then
    return 1
 fi
 
-BRANCH=2.2.15
+BRANCH=indigo-devel
 echo "---------------------------------------------------------------------------------------------------"
-echo "-------------------------------------pull realsense-ros--------------------------------------------"
+echo "-------------------------------------pull apriltags-ros--------------------------------------------"
 echo "---------------------------------------------------------------------------------------------------"
-cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/apriltags-ros
 git checkout $BRANCH
 git pull
 
@@ -74,12 +74,11 @@ if [ "$CONFLICTS" -gt 0 ] ; then
    return 1
 fi
 
-BRANCH=indigo-devel
 echo "---------------------------------------------------------------------------------------------------"
-echo "-------------------------------------pull apriltags-ros--------------------------------------------"
+echo "-------------------------------------pull realsense-ros--------------------------------------------"
 echo "---------------------------------------------------------------------------------------------------"
-cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/apriltags-ros
-git checkout $BRANCH
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
+git checkout 2.2.15
 git pull
 
 CONFLICTS=$(git ls-files -u | wc -l)
