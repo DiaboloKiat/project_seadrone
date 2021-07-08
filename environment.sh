ed98f00ffbeae416d63616a30d88f91386a4c646
DiaboloKiat <DiaboloKiat@gmail.com>
2021-07-08 15:30:40 +0800

    organize files on project_seadrone

--- environment.sh
+++ environment.sh
@@ -1,3 +1,6 @@
+#! /bin/bash
+
+source ~/.bashrc
 source ~/project_seadrone/catkin_ws/devel/setup.bash
-source /opt/ros/melodic/setup.bash --extend
+
 
--- github/git-pull-master.sh
+++ github/git-pull-master.sh
@@ -8,9 +8,9 @@ if [ ! -z "$1" ]; then
     BRANCH=$1
 fi
 
-echo "--------------------------------------------------------------------------------"
-echo "---------------------------pull project_seadrone--------------------------------"
-echo "--------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+echo "------------------------------------pull project_seadrone------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
 git pull
 
 CONFLICTS=$(git ls-files -u | wc -l)
@@ -19,9 +19,9 @@ if [ "$CONFLICTS" -gt 0 ] ; then
    return 1
 fi
 
-echo "-----------------------------------------------------------------------"
-echo "--------------------------pull seadrone_base----------------------------"
-echo "-----------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+echo "---------------------------------------pull seadrone_base------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
 cd ~/project_seadrone/catkin_ws/src/seadrone_base
 git checkout $BRANCH
 git pull
@@ -37,6 +37,48 @@ echo "--------------------------------------------------------------------------
 echo "--------------------------pull hand-gesture-recognition-using-mediapipe----------------------------"
 echo "---------------------------------------------------------------------------------------------------"
 cd ~/project_seadrone/catkin_ws/src/hand-gesture-recognition-using-mediapipe
+git checkout $BRANCH
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in seadrone_base. Aborting"
+   return 1
+fi
+
+BRANCH=melodic
+echo "---------------------------------------------------------------------------------------------------"
+echo "-------------------------------------pull vision_opencv--------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/vision_opencv
+git checkout $BRANCH
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in seadrone_base. Aborting"
+   return 1
+fi
+
+BRANCH=2.2.15
+echo "---------------------------------------------------------------------------------------------------"
+echo "-------------------------------------pull realsense-ros--------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
+git checkout $BRANCH
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in seadrone_base. Aborting"
+   return 1
+fi
+
+BRANCH=indigo-devel
+echo "---------------------------------------------------------------------------------------------------"
+echo "-------------------------------------pull apriltags-ros--------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/apriltags-ros
 git checkout $BRANCH
 git pull
 
