9e49e671c9ec064ae37ba3e4a0aa59457c1652e3
DiaboloKiat <DiaboloKiat@gmail.com>
2021-07-08 20:24:50 +0800

    update submodule on project_seadrone

--- github/git_branch.sh
+++ github/git_branch.sh
@@ -11,10 +11,10 @@ git checkout main
 cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/vision_opencv
 git checkout melodic
 
-cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
-git checkout 2.2.15
-
 cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/apriltags-ros
 git checkout indigo-devel
+
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
+git checkout 2.2.15
 
 cd ~/project_seadrone
