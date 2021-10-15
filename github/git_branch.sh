19ac2d7b00d36b4049a9e9ffecd890533aa87df6
DiaboloKiat <DiaboloKiat@gmail.com>
2021-10-15 22:32:21 +0800

    github update on project_seadrone

--- github/git_branch.sh
+++ github/git_branch.sh
@@ -5,16 +5,16 @@ git checkout master
 cd ~/project_seadrone/catkin_ws/src/hand-gesture-recognition-using-mediapipe
 git checkout main
 
-cd ~/project_seadrone/catkin_ws/src/seadrone_base
-git checkout master
+# cd ~/project_seadrone/catkin_ws/src/seadrone_base
+# git checkout master
 
-cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/vision_opencv
-git checkout melodic
+# cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/vision_opencv
+# git checkout melodic
 
-cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/apriltags_ros
-git checkout indigo-devel
+# cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/apriltags_ros
+# git checkout indigo-devel
 
-cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
-git checkout 2.2.15
+# cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
+# git checkout 2.2.15
 
 cd ~/project_seadrone
