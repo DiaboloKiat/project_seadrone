dfd481e4059291e9e4a6e1a05684e8c5e7e09109
DiaboloKiat <DiaboloKiat@gmail.com>
2021-10-15 22:42:33 +0800

    github update

--- github/git_pull.sh
+++ github/git_pull.sh
@@ -33,59 +33,59 @@ if [ "$CONFLICTS" -gt 0 ] ; then
    return 1
 fi
 
-# echo "---------------------------------------------------------------------------------------------------"
-# echo "---------------------------------------pull seadrone_base------------------------------------------"
-# echo "---------------------------------------------------------------------------------------------------"
-# cd ~/project_seadrone/catkin_ws/src/seadrone_base
-# git checkout $BRANCH
-# git pull
+echo "---------------------------------------------------------------------------------------------------"
+echo "---------------------------------------pull seadrone_base------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+cd ~/project_seadrone/catkin_ws/src/seadrone_base
+git checkout $BRANCH
+git pull
 
-# CONFLICTS=$(git ls-files -u | wc -l)
-# if [ "$CONFLICTS" -gt 0 ] ; then
-#    echo "There is conflict in seadrone_base. Aborting"
-#    return 1
-# fi
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in seadrone_base. Aborting"
+   return 1
+fi
 
-# BRANCH=melodic
-# echo "---------------------------------------------------------------------------------------------------"
-# echo "-------------------------------------pull vision_opencv--------------------------------------------"
-# echo "---------------------------------------------------------------------------------------------------"
-# cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/vision_opencv
-# git checkout $BRANCH
-# git pull
+BRANCH=melodic
+echo "---------------------------------------------------------------------------------------------------"
+echo "-------------------------------------pull vision_opencv--------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/vision_opencv
+git checkout $BRANCH
+git pull
 
-# CONFLICTS=$(git ls-files -u | wc -l)
-# if [ "$CONFLICTS" -gt 0 ] ; then
-#    echo "There is conflict in vision_opencv. Aborting"
-#    return 1
-# fi
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in vision_opencv. Aborting"
+   return 1
+fi
 
-# echo "---------------------------------------------------------------------------------------------------"
-# echo "-------------------------------------pull realsense-ros--------------------------------------------"
-# echo "---------------------------------------------------------------------------------------------------"
-# cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
-# git checkout 2.2.15
-# git pull
+echo "---------------------------------------------------------------------------------------------------"
+echo "-------------------------------------pull realsense-ros--------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/realsense-ros
+git checkout 2.2.15
+git pull
 
-# CONFLICTS=$(git ls-files -u | wc -l)
-# if [ "$CONFLICTS" -gt 0 ] ; then
-#    echo "There is conflict in realsense-ros. Aborting"
-#    return 1
-# fi
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in realsense-ros. Aborting"
+   return 1
+fi
 
-# BRANCH=indigo-devel
-# echo "---------------------------------------------------------------------------------------------------"
-# echo "-------------------------------------pull apriltags-ros--------------------------------------------"
-# echo "---------------------------------------------------------------------------------------------------"
-# cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/apriltags_ros
-# git checkout $BRANCH
-# git pull
+BRANCH=indigo-devel
+echo "---------------------------------------------------------------------------------------------------"
+echo "-------------------------------------pull apriltags-ros--------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------"
+cd ~/project_seadrone/catkin_ws/src/seadrone_base/sensors/apriltags_ros
+git checkout $BRANCH
+git pull
 
-# CONFLICTS=$(git ls-files -u | wc -l)
-# if [ "$CONFLICTS" -gt 0 ] ; then
-#    echo "There is conflict in apriltags-ros. Aborting"
-#    return 1
-# fi
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in apriltags-ros. Aborting"
+   return 1
+fi
 
 cd ~/project_seadrone
 return 0
